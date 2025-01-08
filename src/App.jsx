e2f7f2dbd7902af4828ccba6107fa99c3ef0d762
Charlie Bird <cbvvrd@gmail.com>
2025-01-08 15:59:25 +0000

    installed Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+
+function App() {
+  return (
+    <div className="bg-sky-700">
+      <Header />
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+
+
+function Header() {
+    return (
+        <header>
+            <h1>Charlie Bird - Developer</h1>
+            <button class="menu-button">Menu</button>
+            <nav class="links">
+                <a href="#about">About</a>
+                <a href="#portfolio">Portfolio</a>
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
