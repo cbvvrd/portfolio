e5060dca8ed2875935847c825155ac25f7e04514
Charlie Bird <cbvvrd@gmail.com>
2025-01-09 13:19:04 +0000

    menu links toggle function

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react"
 
 
 function Header() {
+
+    const [hiddenLinks, setHiddenLinks] = useState(true);
+
+    function toggleMenu() {
+        hiddenLinks ? setHiddenLinks(false) : setHiddenLinks(true);
+    }
+
+
     return (
         <header className="bg-sky-300 p-1 border-b border-black flex flex-col items-center">
             <h1 className="text-3xl p-1 text-wrap">Charlie Bird - Developer</h1>
-            <button className="bg-black text-white rounded-md p-1">Menu</button>
-            <nav className="flex flex-row gap-2 text-lg">
+            <button onClick={toggleMenu} className="bg-black text-white rounded-md p-1">Menu</button>
+            <nav className={hiddenLinks ? "hidden " : "flex flex-row gap-2 text-lg"} >
                 <a href="#about" className="border-b-2 border-black">About</a>
                 <a href="#portfolio" className="border-b-2 border-black">Portfolio</a>
                 <a href="#contact" className="border-b-2 border-black">Contact</a>
             </nav>
         </header>
-    )
+    );
 }
  
 export default Header
+
+
+// 
