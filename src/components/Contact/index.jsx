523f36524b1f22cf621406a4b94005170581b70c
Charlie Bird <cbvvrd@gmail.com>
2025-01-13 19:22:50 +0000

    got modal box initial display logic added

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,10 +4,10 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Contact() {
     return(
-        <div className="flex flex-col justify-evenly items-center gap-3 mx-12 my-6 border-b border-black font-questrial">
+        <div id="contact" className="flex flex-col justify-evenly items-center gap-3 mx-12 my-6 border-b border-black font-questrial">
             <div className="flex flex-col justify-evenly items-center gap-3">
                 <h2 className="bg-black text-white text-2xl text-center p-3">Contact:</h2>
-                <p>If you like what you see or have any questions please feel free to contact me. I can be reached using any of the below methods.</p>
+                <p className="text-center">If you like what you see or have any questions please feel free to contact me. I can be reached using any of the below methods.</p>
             </div>
             <div className="flex flex-col gap-2 underline underline-offset-4">
                 <a href="#">Email@ <FontAwesomeIcon icon={faEnvelope}/></a>
--- src/components/PortfolioGallery/index.jsx
+++ src/components/PortfolioGallery/index.jsx
@@ -14,20 +14,37 @@ function PortfolioGallery() {
         });
     }, []);
 
+    const [modal, setModal] = useState(true);
+
+    function toggleModal() {
+        setModal(!modal);
+        console.log("Box is " + modal);
+    }
+
+    // useEffect(toggleDialog, []);
+
     return(
-        <div>
+        <div className="flex flex-col m-auto mb-6 gap-5">
             {projects.map(project => 
-                <div>
-                    <h3>{project.name}</h3>
+                <div className="bg-sky-100 py-3 px-5 border border-black rounded-sm">
+                    <h3 className="p-1 text-xl font-bold">{project.name}</h3>
                     <img src={project.img} alt={project.alt_text}/>
-                    <div>
-                        <button>Info</button>
+                    <div className="flex justify-between p-2">
+                        <button onClick={toggleModal} className="bg-slate-50 border border-black px-2 rounded">Info</button>
                         <a href={project.site_link} target="_blank">Live site</a>
                         <a href={project.github_link} target="_blank">Github<FontAwesomeIcon icon={faGithub} /></a>
                         
                     </div>
                 </div>
             )}
+            {modal && 
+                <div onClick={toggleModal} className="modal-backdrop fixed top-0 left-0 right-0 w-full h-full bg-gray-400/25">
+                    <div className="modal-content fixed top-10 left-10 right-10 bg-red-500">
+                        <p>Hello</p>
+                        <p>Is this a box yet?</p>
+                    </div>
+                </div>
+            }
         </div>
     );
 }
