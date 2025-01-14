5e2652e349d7e4eb69063ed7623d60f8161c1777
Charlie Bird <cbvvrd@gmail.com>
2025-01-14 15:16:45 +0000

    modal boxes now fully functioning with correct info per project

--- src/components/PortfolioGallery/index.jsx
+++ src/components/PortfolioGallery/index.jsx
@@ -21,33 +21,34 @@ function PortfolioGallery() {
 
     function toggleModal() {
         setModal(!modal);
-        console.log("Box is " + modal);
     }
-
-    // console.log(projects[1]["name"]);
-    // useEffect(toggleDialog, []);
 
     return(
         <div className="flex flex-col m-auto mb-6 gap-5">
             {/* <p>{projects[1]["name"]}</p> */}
             {projects.map(project => 
-                <div className="bg-sky-100 py-3 px-5 border border-black rounded-sm">
+                <div key={project.id} className="bg-sky-100 py-3 px-5 border border-black rounded-sm">
                     <h3 className="p-1 text-xl font-bold">{project.name}</h3>
                     <img src={project.img} alt={project.alt_text}/>
                     <div className="flex justify-between p-2">
                         <button onClick={() => {toggleModal(); setProject_id(project.id)}} className="bg-slate-50 border border-black px-2 rounded">Info</button>
                         <a href={project.site_link} target="_blank">Live site</a>
-                        <a href={project.github_link} target="_blank">Github<FontAwesomeIcon icon={faGithub} /></a>
+                        <a href={project.github_link} target="_blank">Github <FontAwesomeIcon icon={faGithub} /></a>
                         
                     </div>
                 </div>
             )}
             {modal && 
-                <div onClick={toggleModal} className="modal-backdrop fixed top-0 left-0 right-0 w-full h-full bg-gray-400/25">
-                    <div className="modal-content fixed top-10 left-10 right-10 bg-red-500">
-                        <p>Hello</p>
-                        <p>Is this a box yet?</p>
-                        <PortfolioModal project_id={project_id}/>
+                <div onClick={toggleModal} className="modal-backdrop fixed top-0 left-0 right-0 w-full h-full bg-gray-400/55">
+                    <div onClick={e => e.stopPropagation()} className="modal-content fixed top-10 left-10 right-10 bg-red-500">
+                        <PortfolioModal 
+                            project_id={project_id} 
+                            image={projects[project_id-1]["img"]}
+                            alt={projects[project_id-1]["alt_text"]}
+                            info={projects[project_id-1]["info"]}
+                            site={projects[project_id-1]["site_link"]}
+                            github={projects[project_id-1]["github_link"]}
+                        />
                     </div>
                 </div>
             }
--- src/components/PortfolioModal/index.jsx
+++ src/components/PortfolioModal/index.jsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-
-function PortfolioModal({project_id}) {
+function PortfolioModal({image, alt, info, site, github}) {
     return(
         <div>
-            <p>{project_id}</p>
+            <img src={image} alt={alt}/>
+            <p>{info}</p>
+            <a href={site}>Live site</a>
+            <a href={github}>Github <FontAwesomeIcon icon={faGithub}/></a>
         </div>
     )
 }
